9f38c1963f5e36850f6009cb6b67ff2b795c2384
rm-a0 <michalrepcik9@gmail.com>
2024-05-03 21:42:26 +0200

    added comments

--- avpl_thrd.c
+++ avpl_thrd.c
@@ -1,9 +1,14 @@
+/* avpl_thrd.c
+ * ----------------------
+ * Author:  Michal Repcik
+ * Date:    05.03.2024
+*/
 #include <stdio.h>
 #include <ncurses.h>
 
 #include "avpl_thrd.h"
 
-// Unpacks arguments and passes them into another function
+// Unpacks arguments and passes to play_video function
 void *video_thread(void *args) {
     thrd_args_t *thrd_args = (thrd_args_t*)args;
 
