d83a77c2ee8adf190f50847dffd57d3b544065f3
rm-a0 <michalrepcik9@gmail.com>
2024-05-07 20:57:12 +0200

    commented functions

--- media_proc.h
+++ media_proc.h
@@ -6,9 +6,36 @@
 #ifndef MEDIA_PROC_H__
 #define MEDIA_PROC_H__
 
-#include <ncurses.h>                // Lib for terminal operations
+#include <ncurses.h>    // Lib for terminal operations
 
+/* Function: play_video
+ * --------------------
+ * 
+ * 
+ * Parameters
+ * ----------
+ * vid_title - path to the video that should be played
+ * main_win - window where video should be displayed
+ * 
+ * Return value
+ * ------------
+ * void
+*/
 void play_video(char *vid_title, WINDOW *main_win);
+
+/* Function: display_error
+ * -----------------------
+ * Displays error message on specified window
+ * 
+ * Parameters
+ * ----------
+ * win - pointer to a window where the message should be displayed
+ * msg - message that should be displayed
+ * 
+ * Return value
+ * ------------
+ * void
+*/
 void display_error(WINDOW *win, const char *msg);
 
 #endif // MEDIA_PROC_H__
