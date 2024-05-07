48de8c5043556883586d80674bbd7c8fdd902a46
rm-a0 <michalrepcik9@gmail.com>
2024-05-07 20:44:26 +0200

    added comments for functions

--- ascii_conv.h
+++ ascii_conv.h
@@ -9,6 +9,21 @@
 #include <ncurses.h>                // Lib for terminal operations
 #include <libavformat/avformat.h>   // Lib for multimedia containers
 
+/* Function: frame_to_ascii
+ * ------------------------
+ * Function that converts frame to ascii art
+ * 
+ * Parameters
+ * ----------
+ * win - window where the frame is supposed to be displayed
+ * frame - video frame that needs to be converted
+ * w_height - height of the window (win)
+ * w_width - width of the window (win)
+ * 
+ * Return value
+ * ------------
+ * void
+*/
 void frame_to_ascii(WINDOW *win, AVFrame *frame, int w_height, int w_width);
 
 #endif // ASCII_CONV_H__
